01c05d93d05e6978ba9cbb26042cde366951ba40
michal.nawilny <michal.nawilny@tesco.com>
2023-12-09 09:27:26 +0100

    day 09

--- src/main/kotlin/com/nawilny/aoc2023/day09/Day09.kt
+++ src/main/kotlin/com/nawilny/aoc2023/day09/Day09.kt
@@ -1,7 +1,32 @@
 package com.nawilny.aoc2023.day09
 
 import com.nawilny.aoc2023.commons.Input
+import com.nawilny.aoc2023.commons.Input.println
 
 fun main() {
-    val input = Input.readFileLinesNormalized("day09", "example.txt")
+    val input = Input.readFileLinesNormalized("day09", "input.txt")
+        .map { l -> l.split(" ").map { it.toLong() } }
+
+    input.sumOf { extrapolate(it) { value, list -> value + list.last() } }.println()
+    input.sumOf { extrapolate(it) { value, list -> list.first() - value } }.println()
 }
+
+private fun extrapolate(l: List<Long>, nexValueGenerator: (Long, List<Long>) -> Long): Long {
+    val steps = mutableListOf<List<Long>>()
+    steps.add(l)
+    while (steps.last().any { it != 0L }) {
+        steps.add(generateDifferences(steps.last()))
+    }
+    return steps.asReversed().fold(0L, nexValueGenerator)
+}
+
+private fun generateDifferences(l: List<Long>): List<Long> {
+    var last = l.first()
+    val result = mutableListOf<Long>()
+    l.drop(1).forEach {
+        result.add(it - last)
+        last = it
+    }
+    return result
+}
+

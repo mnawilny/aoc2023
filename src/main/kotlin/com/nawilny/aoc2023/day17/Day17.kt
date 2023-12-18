1eedbeb9477608d461afbee4cb28ccc8e8f4aab4
michal.nawilny <michal.nawilny@tesco.com>
2023-12-18 08:59:11 +0100

    day 17 and 18

--- src/main/kotlin/com/nawilny/aoc2023/day17/Day17.kt
+++ src/main/kotlin/com/nawilny/aoc2023/day17/Day17.kt
@@ -1,7 +1,90 @@
 package com.nawilny.aoc2023.day17
 
 import com.nawilny.aoc2023.commons.Input
+import com.nawilny.aoc2023.commons.Input.println
 
 fun main() {
-    val input = Input.readFileLinesNormalized("day17", "example.txt")
+    val input = Input.readFileLinesNormalized("day17", "input.txt")
+
+    val map = Map(input)
+    val start = System.currentTimeMillis()
+    getMinHeatLossPath(map, 1..3).println()
+    println("Part 1 calculated in ${(System.currentTimeMillis() - start) / 1000.0}s")
+    getMinHeatLossPath(map, 4..10).println()
+    println("Part 2 calculated in ${(System.currentTimeMillis() - start) / 1000.0}s")
+}
+
+private fun getMinHeatLossPath(map: Map, possibleSteps: IntRange): Int {
+    val visitedPositions = mutableSetOf<Position>()
+    val distances = mutableMapOf<Position, Int>()
+
+    // workaround to attempt going both down and right at the start
+    distances[Position(Point(0, 0), Direction.LEFT)] = 0
+    distances[Position(Point(0, 0), Direction.UP)] = 0
+
+    while (true) {
+        val current = distances.filter { !visitedPositions.contains(it.key) }.minBy { it.value }
+        val currentPosition = current.key
+        val currentDistance = current.value
+        if (currentPosition.p == map.end) {
+            return currentDistance
+        }
+        generateNextMoves(currentPosition, possibleSteps).forEach { next ->
+            val losses = next.map { map.getHeatLoss(it.p) }
+            if (losses.all { it != null }) {
+                val distance = currentDistance + losses.sumOf { it!! }
+                if (!distances.contains(next.last()) || distances[next.last()]!! > distance) {
+                    distances[next.last()] = distance
+                }
+            }
+        }
+        visitedPositions.add(currentPosition)
+    }
+}
+
+private enum class Direction {
+    UP, DOWN, LEFT, RIGHT
+}
+
+private val rotations = mapOf(
+    Direction.UP to listOf(Direction.LEFT, Direction.RIGHT),
+    Direction.DOWN to listOf(Direction.LEFT, Direction.RIGHT),
+    Direction.LEFT to listOf(Direction.UP, Direction.DOWN),
+    Direction.RIGHT to listOf(Direction.UP, Direction.DOWN)
+)
+
+private fun generateNextMoves(position: Position, possibleSteps: IntRange): List<List<Position>> {
+    return rotations[position.d]!!.flatMap { r ->
+        possibleSteps.map { n ->
+            (1..n).map { position.move(r, it) }
+        }
+    }
+}
+
+private data class Point(val x: Int, val y: Int)
+
+private data class Position(val p: Point, val d: Direction) {
+    fun move(d: Direction, n: Int): Position {
+        val nextP = when (d) {
+            Direction.UP -> Point(p.x, p.y - n)
+            Direction.DOWN -> Point(p.x, p.y + n)
+            Direction.LEFT -> Point(p.x - n, p.y)
+            Direction.RIGHT -> Point(p.x + n, p.y)
+        }
+        return Position(nextP, d)
+    }
+}
+
+private class Map(input: List<String>) {
+    private val heatLoss = input.map { line -> line.map { it.digitToInt() } }
+
+    val end = Point(input[0].length - 1, input.size - 1)
+
+    fun getHeatLoss(p: Point): Int? {
+        return if (p.x < 0 || p.y < 0 || p.y >= heatLoss.size || p.x >= heatLoss[p.y].size) {
+            null
+        } else {
+            heatLoss[p.y][p.x]
+        }
+    }
 }
--- src/main/kotlin/com/nawilny/aoc2023/day18/Day18.kt
+++ src/main/kotlin/com/nawilny/aoc2023/day18/Day18.kt
@@ -1,7 +1,126 @@
 package com.nawilny.aoc2023.day18
 
 import com.nawilny.aoc2023.commons.Input
+import com.nawilny.aoc2023.commons.Input.println
 
 fun main() {
-    val input = Input.readFileLinesNormalized("day18", "example.txt")
+    val input = Input.readFileLinesNormalized("day18", "input.txt")
+
+    val trench = Trench()
+    input.map { parseCommand(it) }.forEach { trench.dig(it) }
+    trench.volume().println()
+
+    val trench2 = Trench()
+    input.map { parseColourCommand(it) }.forEach { trench2.dig(it) }
+    trench2.volume().println()
+}
+
+private class Trench {
+    private data class VLine(val x: Int, val y1: Int, val y2: Int)
+
+    private val trenchLines = mutableSetOf<VLine>()
+    private var currentPosition = Point(0, 0)
+
+    fun dig(c: Command) {
+        when (c.direction) {
+            Direction.U -> {
+                trenchLines.add(VLine(currentPosition.x, currentPosition.y - c.distance, currentPosition.y))
+                currentPosition = Point(currentPosition.x, currentPosition.y - c.distance)
+            }
+
+            Direction.D -> {
+                trenchLines.add(VLine(currentPosition.x, currentPosition.y, currentPosition.y + c.distance))
+                currentPosition = Point(currentPosition.x, currentPosition.y + c.distance)
+            }
+
+            Direction.L -> {
+                currentPosition = Point(currentPosition.x - c.distance, currentPosition.y)
+            }
+
+            Direction.R -> {
+                currentPosition = Point(currentPosition.x + c.distance, currentPosition.y)
+            }
+        }
+    }
+
+    fun volume(): Long {
+        var volume = 0L
+        for (y in trenchLines.minOf { it.y1 }..trenchLines.maxOf { it.y2 }) {
+            val yLines = trenchLines.filter { it.y1 <= y && it.y2 >= y }.sortedBy { it.x }
+            var inside = false
+            var insideLine = false
+            var x = Long.MIN_VALUE
+
+            var lineVolume = 0L
+            var previousLine: VLine? = null
+
+            fun handleHorizontalLine(line: VLine, changeInsidePredicate: () -> Boolean) {
+                if (insideLine) {
+                    lineVolume += line.x - x
+                    insideLine = false
+                    if (changeInsidePredicate()) {
+                        inside = !inside
+                    }
+                } else {
+                    if (inside) {
+                        lineVolume += line.x - x
+                    } else {
+                        lineVolume += 1
+                    }
+                    insideLine = true
+                }
+            }
+
+            fun handleVerticalLine(line: VLine) {
+                if (inside) {
+                    lineVolume += line.x - x
+                } else {
+                    lineVolume += 1
+                }
+                inside = !inside
+            }
+
+            yLines.forEach { line ->
+                when (y) {
+                    line.y1 -> handleHorizontalLine(line) { y == previousLine!!.y2 }
+                    line.y2 -> handleHorizontalLine(line) { y == previousLine!!.y1 }
+                    else -> handleVerticalLine(line)
+                }
+                x = line.x.toLong()
+                previousLine = line
+            }
+            volume += lineVolume
+        }
+        return volume
+    }
+
+}
+
+private data class Point(val x: Int, val y: Int)
+
+private enum class Direction {
+    L, R, U, D
+}
+
+private data class Command(val direction: Direction, val distance: Int)
+
+private fun parseCommand(l: String): Command {
+    val parts = l.split(" ")
+    val direction = Direction.valueOf(parts[0])
+    val distance = parts[1].toInt()
+    return Command(direction, distance)
+}
+
+private fun parseColourCommand(l: String): Command {
+    val parts = l.split(" ")
+    var colour = parts[2].substring(2..parts[2].length - 2)
+    val direction = when (colour.last()) {
+        '0' -> Direction.R
+        '1' -> Direction.D
+        '2' -> Direction.L
+        '3' -> Direction.U
+        else -> error("Invalid direction ${colour.last()}")
+    }
+    val distance = colour.dropLast(1).toInt(radix = 16)
+    return Command(direction, distance)
 }
